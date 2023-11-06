ab6f6bf88fbb100db558f66ee73082f4fda23c7d
Udayansp <131608942+Udayansp@users.noreply.github.com>
2023-11-06 16:16:06 +0530

    app

--- apii.py
+++ apii.py
@@ -39,8 +39,8 @@ def predict():
     else:
         return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     app.run(debug=False,host='0.0.0.0', port=8080)
 
 
 
